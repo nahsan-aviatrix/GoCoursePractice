c05f22861cc7ac6d6348e9da7f1ab54df382a577
nafisahsan <nahsan@aviatrix.com>
2022-05-06 13:32:19 -0700

    testing using imported modules

---
+++ deck_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	// check that a deck is created with right num of cards
+	// check that first card is correct
+	// check if last card is correct
+	// OVERALL: checking that we are getting correct output, by checking our assertions about different things.
+	// In this case we can check deck size, first card value last card value, etc.
+
+	// mod file responsible for
+	// mod
+
+	d := newDeck()
+
+	if len(d) != 16 {
+		// notify t something went wrong:
+		t.Errorf("%vExpected deck length of 20, but got:", len(d))
+	}
+}
+
+// func TestSaveToDeckandNewDeckFromFile() {
+
+// }
